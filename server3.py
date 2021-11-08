b4daefb4f50d85cc367e499310af5c77c1836f5e
Neha275 <neha.singh@knoldus.com>
2021-11-08 09:44:37 +0530

    initial commit

---
+++ server3.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def router():
+       return 'Hello from server3'
+if __name__== '__main__':
+    app.run(host = "0.0.0.0",port=5003)
